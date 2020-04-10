62f5af2a0a1eaf652fc5b9c8a7ad6b5ef1a14b3b
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-04-10 14:25:54 +0200

    JSON update + TODOs for main flow + getByTagMethod

--- conversationgraph.py
+++ conversationgraph.py
@@ -2,6 +2,30 @@ import json
 from typing import List
 
 
+class Corpus:
+    def __init__(self,
+                 tag: str,
+                 keywords: List[str],
+                 tagsToAchieveBefore: List[str],
+                 nextTags: List[str],
+                 tagsToCloseWhenDone: List[str],
+                 isClosed: bool,
+                 isAchieved: bool,
+                 redirectTo: str,
+                 patterns: List[str],
+                 responses: List[str]):
+        self.tag = tag
+        self.keywords = keywords
+        self.tagsToAchieveBefore = tagsToAchieveBefore
+        self.nextTags = nextTags
+        self.tagsToCloseWhenDone = tagsToCloseWhenDone
+        self.isClosed = isClosed
+        self.isAchieved = isAchieved
+        self.redirectTo = redirectTo
+        self.patterns = patterns
+        self.responses = responses
+
+
 class ConversationGraph:
     def __init__(self, path: str):
         with open(path) as json_file:
@@ -17,29 +41,16 @@ class ConversationGraph:
                         tag["tagsToCloseWhenDone"],
                         tag["isClosed"],
                         tag["isAchieved"],
+                        tag["redirectTo"],
                         tag["patterns"],
                         tag["responses"]
                     )
                 )
 
-
-class Corpus:
-    def __init__(self,
-                 tag: str,
-                 keywords: List[str],
-                 tagsToAchieveBefore: List[str],
-                 nextTags: List[str],
-                 tagsToCloseWhenDone: List[str],
-                 isClosed: bool,
-                 isAchieved: bool,
-                 patterns: List[str],
-                 responses: List[str]):
-        self.tag = tag
-        self.keywords = keywords
-        self.tagsToAchieveBefore = tagsToAchieveBefore
-        self.nextTags = nextTags
-        self.tagsToCloseWhenDone = tagsToCloseWhenDone
-        self.isClosed = isClosed
-        self.isAchieved = isAchieved
-        self.patterns = patterns
-        self.responses = responses
+    def getByTag(self, tag: str) -> Corpus:
+        for x in self.corpuses:
+            if x.tag == tag:
+                break
+        else:
+            x = None
+        return x
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-from conversationgraph import ConversationGraph
+from typing import Dict
+from conversationgraph import ConversationGraph, Corpus
+
 print("Hello World")
 
 graph = ConversationGraph("conversations-test.json")
 print(graph.corpuses[0].tag)
+
+input_string: str = ""
+
+# TODO: obtain input string
+# input_string = inputStringObtainer()
+
+tagProbabilities: Dict[str, float]
+
+# TODO: obtain tags probabilities based on input string
+# tagProbabilities = neuralNetworkAnalysis(input_string)
+
+tag: Corpus
+
+# TODO: obtain corpus based on tags probabilities map and searcher result
+# tag = graph.searchTagToReturn(tagProbabilities)
+
+textToPrint: str = ""
+
+# TODO: choose text to print - random from corpus
+# textToPrint = randomizer(tag.responses)
+
+print(textToPrint)
+
+
+
 
