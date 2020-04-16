e6fbd9c4dcc2a783fbe57aaa9944acea7ae93e2a
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-04-16 21:02:04 +0200

    Remove of application as Class

--- application.py
+++ application.py
@@ -1,28 +1,18 @@
+from conversationgraph import ConversationGraph
 from neuralnetwork import NeuralNetwork
 from preprocessing import Preprocessor
-from conversationgraph import ConversationGraph
 
+conversation_graph = ConversationGraph()
+lemmatizer = Preprocessor(conversation_graph.corpuses)
+neural_network = NeuralNetwork(8, conversation_graph)
 
-class Application(object):
+neural_network.train(
+    lemmatizer.prepare_nn_entries(),
+    lemmatizer.lemmatize_all_patterns())
 
-    def __init__(self) -> None:
-        super().__init__()
-        self.conversation_graph = ConversationGraph()
-        self.lemmatizer = Preprocessor(self.conversation_graph.corpuses)
-        self.neural_network = NeuralNetwork(8, self.conversation_graph)
+print("Welcome in our rpg chat bot npc !!!")
 
-    def start(self):
-        self.train_mode()
-        print("Welcome in our rpg chat bot npc !!!")
-        while True:
-            expression = input("Player: ")
-            input_lemmatize = self.lemmatizer.lemmatize_expression(expression)
-            self.neural_network.predict(input_lemmatize)
-
-    def train_mode(self):
-        self.neural_network.train(
-            self.lemmatizer.prepare_nn_entries(),
-            self.lemmatizer.lemmatize_all_patterns())
-
-
-Application().start()
+while True:
+    expression = input("Player: ")
+    input_lemmatize = lemmatizer.lemmatize_expression(expression)
+    neural_network.predict(input_lemmatize)
