ae87b13d1e3c6015dda1427b927356d152751e4e
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-04-17 13:41:37 +0200

    Multiple redirectTo mechanic

--- conversationgraph.py
+++ conversationgraph.py
@@ -1,3 +1,5 @@
+import random
+
 from model import Corpus
 from reader import ConversationGraphJsonReader
 
@@ -16,6 +18,9 @@ class ConversationGraph(ConversationGraphJsonReader):
             x = None
         return x
 
+    def is_available_to_respond(self, tag: str) -> bool:
+        return self.check_if_is_achieved(tag) and not self.get_by_tag(tag).isClosed
+
     def check_if_is_achieved(self, tag: str) -> bool:
         for requirement in self.get_by_tag(tag).tagsToAchieveBefore:
             if not self.get_by_tag(requirement).isAchieved:
@@ -25,10 +30,16 @@ class ConversationGraph(ConversationGraphJsonReader):
     def get_corpus_to_respond(self, tag: str) -> Corpus:
         corpus: Corpus = self.get_by_tag(tag)
 
-        if self.check_if_is_achieved(tag) and not corpus.isClosed:
+        if self.is_available_to_respond(tag):
             return corpus
         else:
-            return self.get_corpus_to_respond(corpus.redirectTo)
+            if len(corpus.redirectTo) == 1:
+                return self.get_corpus_to_respond(corpus.redirectTo[0])
+            else:
+                for corpus_to_go in corpus.redirectTo:
+                    if self.is_available_to_respond(corpus_to_go):
+                        self.get_by_tag(corpus_to_go)
+                return self.get_corpus_to_respond(random.choice(corpus.redirectTo))
 
     def close_corresponding_tags(self, corpus: Corpus):
         for tag in corpus.tagsToCloseWhenDone:
