2a52512867c5fcf03db914c83027b497f9213a69
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-06-04 19:58:00 +0200

    extract main and application.py as function

--- application.py
+++ application.py
@@ -6,28 +6,30 @@ from model import Corpus
 from neuralnetwork import NeuralNetwork
 from preprocessing import Preprocessor
 
-config = SafeConfigParser()
-config.read("config.ini")
 
-conversation_graph = ConversationGraph(config.get("main", "path_to_cg_json"))
-lemmatizer = Preprocessor(conversation_graph.corpuses)
-neural_network = NeuralNetwork(4, conversation_graph)
+def main():
+    config = SafeConfigParser()
+    config.read("config.ini")
 
-neural_network.train(
-    lemmatizer.prepare_nn_entries(),
-    lemmatizer.lemmatize_all_patterns())
+    conversation_graph = ConversationGraph(config.get("main", "path_to_cg_json"))
+    lemmatizer = Preprocessor(conversation_graph.corpuses)
+    neural_network = NeuralNetwork(4, conversation_graph)
 
-print(config.get("strings", "welcome_message"))
+    neural_network.train(
+        lemmatizer.prepare_nn_entries(),
+        lemmatizer.lemmatize_all_patterns())
 
-while True:
-    expression = input(config.get("strings", "player_tag") + ": ")
-    input_lemmatize = lemmatizer.lemmatize_expression(expression)
-    matching_tag: str = neural_network.predict(input_lemmatize)
+    print(config.get("strings", "welcome_message"))
 
-    tag: Corpus = conversation_graph.get_corpus_to_respond(matching_tag)
+    while True:
+        expression = input(config.get("strings", "player_tag") + ": ")
+        input_lemmatize = lemmatizer.lemmatize_expression(expression)
+        matching_tag: str = neural_network.predict(input_lemmatize)
 
-    conversation_graph.close_corresponding_tags(tag)
+        tag: Corpus = conversation_graph.get_corpus_to_respond(matching_tag)
 
-    tag.isAchieved = True
+        conversation_graph.close_corresponding_tags(tag)
 
-    print(config.get("strings", "npc_tag") + ": " + random.choice(tag.responses))
+        tag.isAchieved = True
+
+        print(config.get("strings", "npc_tag") + ": " + random.choice(tag.responses))
