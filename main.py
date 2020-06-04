68027efe0a93d52a35a050dfa5bbe5873692faa9
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-06-04 23:31:45 +0200

    first conversation for tests

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+from typing import List
+
 import application
 
+first_conversation: List[str] = [
+    "Who are you?",
+    "Hi",
+    "What's your name?",
+    "Where are we now?",
+    "Thank you very much",
+    "Is there any job for me here?",
+    "I need some weapons, where i can found those?",
+    "Hello",
+    "I have searched the caves, there are some cult activities there",
+    "Where can I go now?",
+    "Appreciate it",
+]
 application.main([4, 4], 0.5)
